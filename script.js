6f18c5ca1d2ba5042d337dcc91b206a6eaa849ab
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-19 00:01:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius=document.getElementById("radius").textContent;
+	let Volume=document.getElementById("volume");
+	if(isNaN(inputR)|| radius<0){
+		Volume.textContent="NaN";
+	}
+	else{
+	let vol=(4 / 3) * Math.PI * Math.pow(radius, 3);
+	Volume.textContent=`${vol}`
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
